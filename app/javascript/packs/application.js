70e62490d546fb44599ec913ec7a554663044bb5
tmtocb <kittz.esport@gmail.com>
2021-01-14 14:26:11 +0100

    add responsive meta tag for mobiles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,14 +14,8 @@ require("stylesheets/application.scss")
 
 document.addEventListener("turbolinks:load", ()=>{
   $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
   })
 })
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
