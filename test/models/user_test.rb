2144b91b5ad7c6d5393794daae11abee11d38083
tmtocb <kittz.esport@gmail.com>
2021-01-15 11:36:21 +0100

    add test for User validation, set-up User fixture

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(email: "unique@example.com", password: "password", password_confirmation: "password")
+  end
+
+  test 'user should be valid' do 
+    assert @user.valid?
+  end
 end
